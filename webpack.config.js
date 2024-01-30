3e796bc1298f7174003eb6ef717c9be5655898df
Vlad Horobets <vlad_horob@ukr.net>
2024-01-30 23:41:11 +0200

    fix: change webpack config for build correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-const webpack = require('webpack');
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 
 const config = {
   entry: './src/index.js',
+  devServer: {
+	port: 8080,
+	static: {
+		directory: path.join(__dirname, 'public'),
+	}
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -31,7 +37,10 @@ const config = {
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+	  new HTMLWebpackPlugin({
+		  template: './index.html'
+	  }),
   ]
 };
 
